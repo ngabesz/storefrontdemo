1bcb414a1d12ce734a6856f52ed3bbecf1df6ede
istvan.szegedi <istvan.szegedi@shoprenter.hu>
2023-05-05 14:35:09 +0200

    remove setters

--- Checkout/App/src/Domain/Cart.php
+++ Checkout/App/src/Domain/Cart.php
@@ -20,19 +20,9 @@ class Cart
         return $this->cartId;
     }
 
-    public function setCartId(EntityId $cartId): void
-    {
-        $this->cartId = $cartId;
-    }
-
     public function getExternalCartId(): string
     {
         return $this->externalCartId;
-    }
-
-    public function setExternalCartId(string $externalCartId): void
-    {
-        $this->externalCartId = $externalCartId;
     }
 
     public function getCartItem(): CartItem
@@ -40,18 +30,9 @@ class Cart
         return $this->cartItem;
     }
 
-    public function setCartItem(CartItem $cartItem): void
-    {
-        $this->cartItem = $cartItem;
-    }
-
     public function getCartTotal(): float
     {
         return $this->cartTotal;
     }
 
-    public function setCartTotal(float $cartTotal): void
-    {
-        $this->cartTotal = $cartTotal;
-    }
 }
