6faa4fb2e9cdfba16967f78815474a721008db20
istvan.szegedi <istvan.szegedi@shoprenter.hu>
2023-05-05 16:40:58 +0200

    routing

--- Checkout/App/src/Presentation/Api/Controller/CheckoutController.php
+++ Checkout/App/src/Presentation/Api/Controller/CheckoutController.php
@@ -2,7 +2,63 @@
 
 namespace App\Presentation\Api\Controller;
 
-class CheckoutController
-{
+use App\Application\CreateCheckout\CreateCheckoutCommand;
+use Nyholm\Psr7\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
+class CheckoutController extends AbstractController
+{
+    use HandleTrait;
+    public function __construct(MessageBusInterface $messageBus,SerializerInterface $serializer)
+    {
+        $this->messageBus = $messageBus;
+        $this->serializer = $serializer;
+    }
+
+    public function createCheckout(Request $request)
+    {
+        $post = json_decode($request->getContent());
+        $command = new CreateCheckoutCommand($post->cartId);
+
+        $response = $this->handle(
+            $command
+        );
+
+        $jsonresponse = $this->serializer->serialize($response,'json');
+        return new Response($jsonresponse);
+    }
+
+    public function getCheckout(Request $request)
+    {
+
+    }
+
+    public function saveCustomer(Request $request)
+    {
+
+    }
+
+    public function saveShippingAddress(Request $request)
+    {
+
+    }
+
+    public function saveBillingAddress(Request $request)
+    {
+
+    }
+
+    public function savePaymentMethod(Request $request)
+    {
+
+    }
+
+    public function confirm(Request $request)
+    {
+
+    }
 }
