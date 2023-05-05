57bbcad0a11187017091eeba64cc76d766032e41
istvan.szegedi <istvan.szegedi@shoprenter.hu>
2023-05-05 15:52:25 +0200

    orm

--- Checkout/App/src/Domain/BillingAddress.php
+++ Checkout/App/src/Domain/BillingAddress.php
@@ -2,7 +2,42 @@
 
 namespace App\Domain;
 
-class BillingAddress extends Address
+use App\Domain\Shared\EntityId;
+
+class BillingAddress
 {
 
+    public function __construct(
+        private EntityId $addressId,
+        private string $address,
+        private string $country,
+        private string $postcode,
+        private string $city
+    ) {
+    }
+
+    public function getAddressId(): EntityId
+    {
+        return $this->addressId;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function getPostcode(): string
+    {
+        return $this->postcode;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
 }
