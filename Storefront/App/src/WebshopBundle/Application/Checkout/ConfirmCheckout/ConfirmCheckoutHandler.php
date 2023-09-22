260fd18addc272a74df608d8cc7fc4b1708fcb4c
Daniel Bartos <daniel.bartos@shoprenter.hu>
2023-09-22 12:38:59 +0200

    Tipo

--- Storefront/App/src/WebshopBundle/Application/Checkout/ConfirmCheckout/ConfirmCheckoutHandler.php
+++ Storefront/App/src/WebshopBundle/Application/Checkout/ConfirmCheckout/ConfirmCheckoutHandler.php
@@ -2,13 +2,14 @@
 
 namespace App\WebshopBundle\Application\Checkout\ConfirmCheckout;
 
-use App\Application\Exception\ApplicationException;
+use App\WebshopBundle\Application\Cart\Exception\ApplicationException;
 use App\WebshopBundle\Application\Checkout\ConfirmCheckout\Dto\ConfirmCheckoutOutput;
 use App\WebshopBundle\Domain\Exception\DomainException;
 use App\WebshopBundle\Domain\Model\Checkout\CheckoutRepositoryInterface;
 
 class ConfirmCheckoutHandler
 {
+    private CheckoutRepositoryInterface $checkoutRepository;
 
     public function __construct(CheckoutRepositoryInterface $checkoutRepositoryInterface)
     {
